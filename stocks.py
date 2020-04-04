bd8cb166923012b959d65b7d7eb1172b177a0c55
antknees <47506456+antknees@users.noreply.github.com>
2020-04-04 13:18:32 -0700

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -2,16 +2,17 @@ import pyautogui, tkinter, pyperclip, webbrowser, time
 import openpyxl
 import os
 
-#initialize excel file
-os.chdir('E:\Python\Projects')
-wb = openpyxl.load_workbook(filename = 'test.xlsx')
+window = tkinter.Tk()
+window.title("Stocks")
+label = tkinter.Label(window, text = 'Stock Data Entry').pack()
+window.mainloop()
 
 """
-#keep out of loop
-low = []
-high = []
-current = []
-"""
+directory = input('Excel file directory: ')
+#initialize excel file
+os.chdir(directory)
+wb = openpyxl.load_workbook(filename = 'test.xlsx')
+
 
 #loop to fill init list
 loop = True
@@ -22,10 +23,8 @@ while(loop):
     if (ticker == "ZZZZ"):
         break;
     else:
-        tickList.append(ticker)
-#print(tickList)
-        
-#ticker = input("Input the ticker you want to search:")
+        tickList.append(ticker.upper())
+
 r = 1
 for i in tickList:
     #-Beginning of loop with initial list
@@ -33,52 +32,37 @@ for i in tickList:
     pyautogui.moveTo(546, 726)#--------------- 1080 x 1920
     time.sleep(5.2) #lets the webpage load
 
-    #pyautogui.drag(-120, 0, .5, button='left')
     pyautogui.click(clicks = 3)
     pyautogui.hotkey('ctrl', 'c') #takes the hi lo and puts into the clipboard
 
     hilow = pyperclip.paste()
     print(hilow)
 
-    #498 406
     
     split = hilow.split("-")
-    #print(split[0].strip() + " " + split[1].strip())
 
 
     pyautogui.moveTo(498, 406)#------------------1080 x 1920
-    #pyautogui.drag(-180, 0, .5, button = 'left')
     pyautogui.click(clicks = 3)
     pyautogui.hotkey('ctrl', 'c')
 
     price = pyperclip.paste()
-    barePrice = price.replace("-", " ")
-    barePrice = price.replace("+", " ")
-    priceSplit = barePrice.split(" ")
-    print(priceSplit)
-    #low.append(split[0].strip())
-    #high.append(split[1].strip())
+    s = price.find('.')
+    print(s)
+    xPrice = price[:(s+3)]
+    fPrice = xPrice.replace(',','')
+    print(fPrice)
 
-    #high low count
-    #count = 0
-    
-    #row count
+    pyautogui.click(x = 430, y = 15) #close the tab
 
-    #make loop for coloumn/row and make loop for hi low list
-    os.chdir('E:\Python\Projects')
-    wb = openpyxl.load_workbook(filename = 'test.xlsx')
     ws = wb.active
     ws.cell(row = r, column = 1).value = str(tickList[r-1])
-    ws.cell(row = r, column = 2).value = float(priceSplit[0])
-    ws.cell(row = r, column = 3).value = float(split[0].strip())
-    ws.cell(row = r, column = 4).value = float(split[1].strip())
+    ws.cell(row = r, column = 2).value = float(fPrice)
+
+    
+    ws.cell(row = r, column = 3).value = float(split[0].strip().replace(',',''))
+    ws.cell(row = r, column = 4).value = float(split[1].strip().replace(',',''))
     wb.save('test.xlsx')
     
     r += 1
-    
-os.system("taskkill /im firefox.exe /f")
-
-
-
-
-
+"""
